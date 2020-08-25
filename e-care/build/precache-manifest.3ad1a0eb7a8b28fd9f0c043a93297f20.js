66ed8bb4a9bfc0a48f4e7d8723753902f1650e19
Dindu5 <chimdindue@gmail.com>
2020-08-25 09:31:22 +0100

    added build folder

---
+++ e-care/build/precache-manifest.3ad1a0eb7a8b28fd9f0c043a93297f20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81cb29c5130ee025409beb25c208b92d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9380196b2008617bd270",
+    "url": "/static/css/2.493619c7.chunk.css"
+  },
+  {
+    "revision": "d8fab94545b5ca5dd006",
+    "url": "/static/css/main.1967506c.chunk.css"
+  },
+  {
+    "revision": "9380196b2008617bd270",
+    "url": "/static/js/2.7ee6bceb.chunk.js"
+  },
+  {
+    "revision": "a8eb76e4c6beb72c22bcc9f3c026930b",
+    "url": "/static/js/2.7ee6bceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8fab94545b5ca5dd006",
+    "url": "/static/js/main.21f5ce8c.chunk.js"
+  },
+  {
+    "revision": "777266e1fe2e55ef50b5",
+    "url": "/static/js/runtime-main.b16bcf63.js"
+  },
+  {
+    "revision": "78f9efcc4d5b2dfb96c4967934764b5f",
+    "url": "/static/media/illustration.78f9efcc.png"
+  }
+]);
